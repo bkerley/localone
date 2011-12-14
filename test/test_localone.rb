be503a6cf93ccf60ea5189f3a9c7d3c3ac446cea
Bryce Kerley <bkerley@brycekerley.net>
2011-12-13 23:40:23 -0500

    test some more let stuff

--- test/test_localone.rb
+++ test/test_localone.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'helper'
 
 class TestLocalone < Test::Unit::TestCase
@@ -7,7 +8,21 @@ class TestLocalone < Test::Unit::TestCase
     end
   end
 
-  should 'support Kernel.let' do
+  should 'support Kernel.let in a naïve way' do
     assert_equal 'abcdef', let(:a=>'abc', :b=>'def'){ [a, b].join }
+  end
+
+  should 'support Kernel.let with local variables' do
+    canary = rand(36**10).to_s(36)
+    out = let(a: 'aaaa', b: 'bbbb'){ "#{a}#{canary}#{b}"}
+
+    assert_equal "aaaa#{canary}bbbb", out
+  end
+
+  should 'support Kernel.let with instance variables' do
+    @canary = rand(36**10).to_s(36)
+    out = let(a: 'aaaa', b: 'bbbb'){ "#{a}#{@canary}#{b}"}
+
+    assert_equal "aaaa#{@canary}bbbb", out
   end
 end
