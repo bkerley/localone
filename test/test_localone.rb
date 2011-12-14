bbf6403703beb47ed4e9f48648bcdc454d345d4f
Bryce Kerley <bkerley@brycekerley.net>
2011-12-13 23:41:36 -0500

    refactor tests

--- test/test_localone.rb
+++ test/test_localone.rb
@@ -2,27 +2,31 @@
 require 'helper'
 
 class TestLocalone < Test::Unit::TestCase
-  should 'support #with on an array' do
-    %w{ doug bob }.with do |a|
-      assert_equal "doug and bob", a.join(' and ')
+  context 'with' do
+    should 'work on an array' do
+      %w{ doug bob }.with do |a|
+        assert_equal "doug and bob", a.join(' and ')
+      end
     end
   end
 
-  should 'support Kernel.let in a naïve way' do
-    assert_equal 'abcdef', let(:a=>'abc', :b=>'def'){ [a, b].join }
-  end
+  context 'Kernel.let' do
+    should 'support work in a naïve way' do
+      assert_equal 'abcdef', let(:a=>'abc', :b=>'def'){ [a, b].join }
+    end
 
-  should 'support Kernel.let with local variables' do
-    canary = rand(36**10).to_s(36)
-    out = let(a: 'aaaa', b: 'bbbb'){ "#{a}#{canary}#{b}"}
+    should 'work with local variables' do
+      canary = rand(36**10).to_s(36)
+      out = let(a: 'aaaa', b: 'bbbb'){ "#{a}#{canary}#{b}"}
 
-    assert_equal "aaaa#{canary}bbbb", out
-  end
+      assert_equal "aaaa#{canary}bbbb", out
+    end
 
-  should 'support Kernel.let with instance variables' do
-    @canary = rand(36**10).to_s(36)
-    out = let(a: 'aaaa', b: 'bbbb'){ "#{a}#{@canary}#{b}"}
+    should 'work with instance variables' do
+      @canary = rand(36**10).to_s(36)
+      out = let(a: 'aaaa', b: 'bbbb'){ "#{a}#{@canary}#{b}"}
 
-    assert_equal "aaaa#{@canary}bbbb", out
+      assert_equal "aaaa#{@canary}bbbb", out
+    end
   end
 end
